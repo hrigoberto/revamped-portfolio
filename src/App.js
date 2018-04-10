74e975f59fe027eeff7520409496341a45cf566a
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-04-09 18:45:21 -0700

    added some content to a couple of the panels

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {CODE, WELCOME, CONTACT, PHOTOVID, ABOUT } from './components'
+import {CODE, WELCOME, CONTACT, PHOTOVID, ABOUT, NAVBAR } from './components'
+// import WELCOME from './components/welcome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        WELCOME
-        ABOUT
-        PHOTOVID
-        CODE
-        CONTACT
+        <NAVBAR />
+        <WELCOME />
+        <ABOUT />
+        <PHOTOVID />
+        <CODE />
+        <CONTACT />
       </div>
     );
   }
